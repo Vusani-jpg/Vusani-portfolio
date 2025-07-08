f6d725a7081160d529fbd2edba066da0bcc0abe1
Vusani-jpg <matsigilavi@gmail.com>
2025-07-08 15:06:37 +0200

    H

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,10 @@ import Footer from "../../components/sections/Footer";
 export default function Home() {
   return (
     <div className="grid items-center bg-gray-700 justify-items-center min-h-screen gap- sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <div className="absolute inset-0 bg-gradient-to-bl from-background via-background to-primary/5 pointer-events-none overflow-hidden">
-      <div className="absolute top-20 left-10 w-32 h-32 sm:w-48 sm:h-48 lg:w-64 lg:h-64 bg-primary/10 rounded-full blur-3xl animate-pulse" /> */}
+      <div className="absolute inset-0 bg-gradient-to-bl from-background via-background to-primary/5 pointer-events-none overflow-hidden"/>
+      <div className="absolute top-20 left-10 w-32 h-32 sm:w-48 sm:h-48 lg:w-64 lg:h-64 bg-primary/10 rounded-full blur-3xl animate-pulse" />
+
+
       
 			<Navbar />
       <Hero />
@@ -20,10 +22,10 @@ export default function Home() {
       <Projects />
       <Contact />
       <Footer />
-
+</div>
       
-    </div> 
-    // </div> 
+
+    //  </div> 
        
     
 
